5f677c8f1906bcefef49faf1cb94b7bdd03fb732
greyyT <nhannguyen8338@gmail.com>
2024-03-04 17:11:57 +0700

    feat: create navigation bar (not implement authorization)

---
+++ components/navigation/navigation-bar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import Logo from '../logo';
+
+export default function NavigationBar() {
+  return (
+    <nav className="flex justify-center pt-10 px-4">
+      <div className="w-300 max-w-full flex items-center justify-between h-20">
+        <Logo />
+        <div className="flex gap-[30px] font-medium text-gray">
+          <Link href="/generator">Generator</Link>
+          <Link href="/docs">Docs</Link>
+          <button>Login</button>
+          <Link href="/contact">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
