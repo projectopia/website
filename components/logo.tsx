43b62379ae9e17b23214e57725384ba9d656f664
greyyT <nhannguyen8338@gmail.com>
2024-03-14 17:24:08 +0700

    chore: Add priority to Logo image for browser to load it first

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <Image src={projectopiaLogo} alt="projectopiaLogo" width={44} height={44} />
+      <Image src={projectopiaLogo} alt="projectopiaLogo" width={44} height={44} priority />
       <h1 className="text-[26px] leading-[40px] font-bold">
         project<span className="text-purple">opia</span>
       </h1>
