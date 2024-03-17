b6ab5a12cdb7b0066c4a900756f84e589efb4825
greyyT <nhannguyen8338@gmail.com>
2024-03-17 20:26:41 +0700

    chore(input-component): Add variants to input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
+import { VariantProps, cva } from 'class-variance-authority';
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+const inputVariants = cva(
+  'flex w-full text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'bg-background border border-input rounded-md placeholder:text-muted-foreground',
+        ghost: 'rounded-lg placeholder:text-gray bg-generator-form-input border-none',
+      },
+      sizes: {
+        default: 'h-10 px-3 py-2',
+        lg: 'px-[18px] py-[14px] h-auto',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      sizes: 'default',
+    },
+  },
+);
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, variant, sizes, ...props }, ref) => {
+  return <input type={type} className={cn(inputVariants({ variant, sizes, className }))} ref={ref} {...props} />;
 });
 Input.displayName = 'Input';
 
