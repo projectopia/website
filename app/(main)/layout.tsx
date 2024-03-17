d4a42e20ae2a08646a3d87942d2ce2019f12ed6d
greyyT <nhannguyen8338@gmail.com>
2024-03-17 21:11:09 +0700

    feat: Remove fbclid from url if existed

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,27 @@
+'use client';
+
 import { ScrollHandler } from '@/components/scroll-handler';
 import { Footer, NavigationBar } from '@/components/navigation';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default function RootLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Remove fbclid query from search bar
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.delete('fbclid');
+
+    // Redirect to removed fbclid url
+    const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
+    router.push(newUrl);
+  }, [router]);
+
   return (
     <div className="min-h-full">
       <ScrollHandler />
