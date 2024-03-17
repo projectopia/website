105d097b1554302dc5fddbf7eacac9c71245c80f
greyyT <nhannguyen8338@gmail.com>
2024-03-17 16:23:01 +0700

    feat(generator-form): Implement generator form on generator page

--- app/(main)/generator/page.tsx
+++ app/(main)/generator/page.tsx
@@ -1,3 +1,27 @@
+import { GeneratorForm } from '@/components/generator-form';
+import Link from 'next/link';
+
 export default function GeneratorPage() {
-  return <h1>Hello World</h1>;
+  return (
+    <section aria-label="form-generator-section" className="mt-20 flex justify-center px-4">
+      <div className="w-300 max-w-full bg-white rounded-3xl drop-shadow p-20 flex gap-10">
+        <div className="flex-1">
+          <div className="sticky top-20 space-y-5">
+            <h1 className="text-5xl font-bold">Generate Repository</h1>
+            <p className="leading-[26px]">
+              Complete this form to create your project inside your Github Repository using Projectopia&apos;s template.
+              Once finished, you will receive a confirmation email from us.
+            </p>
+            <p className="leading-[26px]">
+              You can also contribute to projectopia’s template collection by checking out this{' '}
+              <Link className="text-purple font-bold" href={''}>
+                link.
+              </Link>
+            </p>
+          </div>
+        </div>
+        <GeneratorForm />
+      </div>
+    </section>
+  );
 }
