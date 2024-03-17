f92dfc5bc097cbd6b022ea6f03e2e403bba8d0f0
greyyT <nhannguyen8338@gmail.com>
2024-03-17 16:25:28 +0700

    chore(generator-form): Modify major css for small desktop screen

--- app/(main)/generator/page.tsx
+++ app/(main)/generator/page.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 export default function GeneratorPage() {
   return (
     <section aria-label="form-generator-section" className="mt-20 flex justify-center px-4">
-      <div className="w-300 max-w-full bg-white rounded-3xl drop-shadow p-20 flex gap-10">
+      <div className="w-300 max-w-full bg-white rounded-3xl drop-shadow p-10 xl:p-20 flex gap-5 xl:gap-10">
         <div className="flex-1">
           <div className="sticky top-20 space-y-5">
-            <h1 className="text-5xl font-bold">Generate Repository</h1>
+            <h1 className="text-4xl xl:text-5xl font-bold">Generate Repository</h1>
             <p className="leading-[26px]">
               Complete this form to create your project inside your Github Repository using Projectopia&apos;s template.
               Once finished, you will receive a confirmation email from us.
