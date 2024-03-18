6b86bedbf3b1b7d8dc2646d0e1281e524a822078
greyyT <nhannguyen8338@gmail.com>
2024-03-18 19:20:02 +0700

    feat(navigation): Change navigation bar functionality
    - Change navigation bar from 'use client' to 'use server'
    - Handle rendering either user's menu or sign in button based on user's loggin status

--- components/navigation/navigation-bar.tsx
+++ components/navigation/navigation-bar.tsx
@@ -1,15 +1,14 @@
-'use client';
+'use server';
 
 import Link from 'next/link';
 
-import { cn } from '@/lib/utils';
+import { auth } from '@/auth';
 import Logo from '@/components/logo';
-import { useModal } from '@/hooks/modal-store';
-import { ChevronRight } from '@/components/icons';
+import { UserCard } from './user-card';
+import { SignInBtn } from './sign-in-btn';
 
-export default function NavigationBar() {
-  // The open modal function from useModal hook
-  const { onOpen } = useModal();
+export default async function NavigationBar() {
+  const session = await auth();
 
   return (
     <nav className="navbar flex justify-center px-4 sticky top-0 transition-all z-40">
@@ -27,21 +26,7 @@ export default function NavigationBar() {
           <li className="hover:text-purple hover:font-semibold transition-[font-weight] duration-200">
             <Link href="/contact">Contact</Link>
           </li>
-          <li>
-            <button
-              className={cn(
-                'group relative bg-purple text-white px-4 py-[6px] rounded-[14px] leading-5 flex items-center gap-2 overflow-hidden',
-                'before:absolute before:bg-[#473797] before:left-0 before:bottom-0 before:w-0 before:h-full before:rounded-[14px]',
-                'before:transition-[width] before:duration-300 hover:before:w-full',
-              )}
-              onClick={() => onOpen('signIn')}
-            >
-              <p className="z-10 pr-4 font-semibold transition-[padding] group-hover:px-2 duration-300">Sign In</p>
-              <div className="z-10 right-4 group-hover:-right-2 transition-[right] duration-300 absolute">
-                <ChevronRight />
-              </div>
-            </button>
-          </li>
+          <li>{session ? <UserCard session={session} /> : <SignInBtn />}</li>
         </ul>
       </div>
     </nav>
